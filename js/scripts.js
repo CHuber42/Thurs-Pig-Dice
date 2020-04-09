d6d6463e15414318ad62b88478774d506067b3ee
Christopher Huber <CHuber42.work@gmail.com>
2020-04-09 13:12:29 -0700

    forgot to commit this morning! added: 2-dice option, computer AI mode. Lots of time spent walking through AI logic/implementation over and over

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+vsWho = "";
+gameVersion = "";
+
 //player constructor
 function player (id){
   this.totalScore = 0; //permanent
@@ -27,7 +30,7 @@ function switchTurns() {
       $("#message").prepend("Player 1, You won the game!");
       $("reset").show();
     }
-    playerTurn = 1;
+    playerTurn = 1; // YOUR TURN, PLAYER 1
   }
   
   else {
@@ -38,10 +41,21 @@ function switchTurns() {
       $("#message").prepend("Player 2, You won the game!");
       $("reset").show();
     }
-    playerTurn = 0;
+    playerTurn = 0; // What does this mean? "YOUR TURN, PLAYER 0"
   }
-  displayScore();
+  displayScore();  
+
+  while(playerTurn == 1 && vsWho == "computer"){
+    versionCheck(); //because it rolled "1" --> go to "switchTurn()"
+    // versionCheck(gameVersion);
+    console.log(allPlayers[1].turnScore, allPlayers[1].totalScore)
   
+
+      if(allPlayers[playerTurn].turnScore > 9){
+      switchTurns();
+      }  
+  }
+
 } 
 
 
@@ -49,7 +63,26 @@ function switchTurns() {
 //################################################################
 //   V Roll Your Dice V
 
+//Add options to play one of the other variations of Pig Dice using two or more dice
+//1. select box for which variants/how many dice. (right now: 2-dice-mode)
+//2. variable to capture/store game version selection
+//3. When we diceroll --> hand game version to function
+//4. do something with diceroll function(?)
+
+
+
+//##################################################
+function versionCheck () {
+if (gameVersion == "one"){
+  diceRoll();
+} 
+else {
+  diceRollTwo();
+}
+}
+
 function diceRoll() {
+  
   var random = Math.floor(Math.random() * 6) + 1;
   $("#rolled-number").text(random);
   if (random == 1){
@@ -63,6 +96,22 @@ function diceRoll() {
  // displayRolledNum();
 }
 
+function diceRollTwo(){
+  var random = Math.floor(Math.random() * 6) + 1;
+  var random2 = Math.floor(Math.random() * 6) + 1;
+
+  $("#rolled-number").text(random + ", " + random2);
+
+  if (random == 1 || random2 == 1){
+    allPlayers[playerTurn].turnScore = 0;
+    switchTurns();
+  }
+  else {
+    allPlayers[playerTurn].turnScore += (random + random2); 
+
+  }
+  displayScore();
+ }
 
 // ^ Roll Your Dice ^
 //##################################################################
@@ -93,7 +142,10 @@ function displayScore() {
 
 $(document).ready(function() {
   $("#roll").click(function(){
-    diceRoll();
+    
+    gameVersion = $("#game-version").val();
+    vsWho = $("#vs-who").val();
+    versionCheck();
   });
   $("#pass").click(function(){
     switchTurns();
@@ -101,7 +153,52 @@ $(document).ready(function() {
   $("#reset").click(function() {
     resetGame();
   });
-  
-  
-  
+     
 });
+
+// Add option to play the computer - easy or hard levels:
+//1. select box to choose to play computer
+//2. store game mode "vs computer or vs player"
+//3. In "Your turn", if "vs computer" then (what?)
+//4. if player1's turn AND vs computer: computer decides player2's functions (Easy: Computer always stops after second roll.) 
+
+
+// Easy: Computer always stops after second roll.
+
+// Hard: Computer uses strategy based on current total and rolled dice.
+
+// if (playerTurn == 1 && vsWho == "computer"){
+//   // computer roll once and pass
+//   do versionCheck(); //This will roll the dice for you. This is diceroll()
+  
+//   //if score is 9 or more, pass, else, roll again
+//   if (allPlayers[1]turnScore >= 9){
+//     switchTurns()<--
+//   }
+//   //do something with function player2 roll
+  
+// }
+
+
+// 1.COMPUTER's Turn <--; "playerTurn = 0" <-- this means it's Chisato's turn // if (playerTurn !== 1)
+//   A. Decide whether to roll or pass // ALWAYS ROLL AS FIRST THING (if turnscore > 9) PASS/ELSE ROLL AGAIN
+//     1. ROLL <-- VersionCheck()
+//     2. AddScore or //rolled 1 - lose turn?
+//   B. PASS <-- switchTurns() | "playerTurn = 0!"
+
+  // if (playerTurn == 1 && vsWho == "computer"){
+  //   versionCheck();
+  //   //CHECK IF THIS ROLLED A 1 AND DON'T CONTINUE
+
+  //     if(allPlayers[playerTurn].turnScore < 9){
+  //     versionCheck();
+  //     }
+  //     else {
+  //     switchTurns();
+  //     }
+  // }
+  
+
+  
+  
+    
